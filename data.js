6703de59c933c6a770c6ef8e0f3bce0f728b1124
Алексей Даньев <aleksejdanev@MacBook-Pro-Aleksej-2.local>
2022-08-23 22:45:18 +0300

    add data

---
+++ data.js
@@ -0,0 +1,89 @@
+export const RIGS = [
+    [
+      {
+        id: `0`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `1`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `2`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `3`, model: `Nvidia RTX 3070m`, core: `0`, memory: `3300`, power: `0`, fan: `0`, temp: `57`, set: `oc --fixclk=1150 --fixpstate=1`
+      },
+      {
+        id: `4`, model: `Nvidia RTX 3070m`, core: `0`, memory: `3300`, power: `0`, fan: `0`, temp: `57`, set: `oc --fixclk=1150 --fixpstate=1`
+      },
+      {
+        id: `5`, model: `Nvidia RTX 3070m`, core: `0`, memory: `3300`, power: `0`, fan: `0`, temp: `57`, set: `oc --fixclk=1150 --fixpstate=1`
+      }
+    ],
+    [
+      {
+        id: `0`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `1`, model: `Gigabyte RTX 3060 Ti`, core: `0`, memory: `2250`, power: `135`, fan: `60`, temp: `57`, set: ``
+      },
+      {
+        id: `2`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `3`, model: `Gigabyte RTX 3060 Ti`, core: `0`, memory: `2100`, power: `130`, fan: `60`, temp: `57`, set: ``
+      },
+      {
+        id: `4`, model: `Gigabyte RTX 3060 Ti`, core: `0`, memory: `2730`, power: `135`, fan: `60`, temp: `57`, set: ``
+      },
+      {
+        id: `5`, model: `Nvidia RTX 3070m`, core: `0`, memory: `3300`, power: `0`, fan: `0`, temp: `57`, set: `oc --fixclk=1150 --fixpstate=1`
+      }
+    ],
+    [
+      {
+        id: `0`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `1`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `2`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `3`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `4`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `5`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      },
+      {
+        id: `6`, model: `Nvidia CMP 90HX`, core: `0`, memory: `1900`, power: `230`, fan: `0`, temp: `0`, set: ``
+      },
+      {
+        id: `7`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+      }
+    ]
+]
+
+export const RIG_2 = [
+  {
+    id: `0`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+  },
+  {
+    id: `1`, model: `Gigabyte RTX 3060 Ti`, core: `0`, memory: `2250`, power: `135`, fan: `60`, temp: `57`, set: ``
+  },
+  {
+    id: `2`, model: `Palit RTX 3070`, core: `0`, memory: `2000`, power: `0`, fan: `60`, temp: `55`, set: `oc -fixpstate=1 -fixclk=1065`
+  },
+  {
+    id: `3`, model: `Gigabyte RTX 3060 Ti`, core: `0`, memory: `2100`, power: `130`, fan: `60`, temp: `57`, set: `oc --fixclk=1150 --fixpstate=1`
+  },
+  {
+    id: `4`, model: `Gigabyte RTX 3060 Ti`, core: `0`, memory: `2730`, power: `135`, fan: `60`, temp: `57`, set: `oc --fixclk=1150 --fixpstate=1`
+  },
+  {
+    id: `5`, model: `Nvidia RTX 3070m`, core: `0`, memory: `3300`, power: `0`, fan: `0`, temp: `57`, set: `oc --fixclk=1150 --fixpstate=1`
+  }
+]
--- src/view/rig.js
+++ src/view/rig.js
@@ -1,80 +1,45 @@
-export const createRigTemplate = () => {
+
+
+const generateCardTuning = (card) => {
+  const {id, model, core, memory, power, fan, temp, set} = card
+
+  return (
+      `<tr>
+           <th class="cell__id" scope="row">${id}</th>
+           <td class="cell__model" contenteditable="true">${model}</td>
+           <td class="cell__core" contenteditable="true">${core}</td>
+           <td class="cell__memory" contenteditable="true">${memory}</td>
+           <td class="cell__power" contenteditable="true">${power}</td>
+           <td class="cell__fan" contenteditable="true">${fan}</td>
+           <td class="cell__temp" contenteditable="true">${temp}</td>
+           <td class="cell__set" contenteditable="true">${set}</td>
+       </tr>`
+  )
+}
+
+export const createRigTemplate = (rig, index) => {
+  const cardItems = rig.map(item => {
+    return generateCardTuning(item)
+  }).join(``)
   return (
     `<article class="rig">
-                <h2 class="rig__title">alex1</h2>
+                <h2 class="rig__title">alex${index}</h2>
                 <table class="table table-bordered">
                     <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Card Model</th>
-                        <th scope="col">Core Clock offset, MHz</th>
-                        <th scope="col">Memory Clock offset, MHz</th>
-                        <th scope="col">Power limit, W</th>
-                        <th scope="col">Fan, %</th>
-                        <th scope="col">Target Temp, С°</th>
-                        <th scope="col">Set</th>
+                        <th class="cell-head__id" scope="col">#</th>
+                        <th class="cell-head__model" scope="col">Card Model</th>
+                        <th class="cell-head__core" scope="col">Core Clock offset, MHz</th>
+                        <th class="cell-head__memory" scope="col">Memory Clock offset, MHz</th>
+                        <th class="cell-head__power" scope="col">Power limit, W</th>
+                        <th class="cell-head__fan" scope="col">Fan, %</th>
+                        <th class="cell-head__temp" scope="col">Target Temp, С°</th>
+                        <th class="cell-head__set" scope="col">Set</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <th scope="row">0</th>
-                        <td contenteditable="true">Mark</td>
-                        <td contenteditable="true">Otto</td>
-                        <td contenteditable="true">@mdo</td>
-                        <td contenteditable="true">Otto</td>
-                        <td contenteditable="true">@mdo</td>
-                        <td contenteditable="true">@mdo</td>
-                        <td contenteditable="true">@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td contenteditable="true">Jacob</td>
-                        <td contenteditable="true">Thornton</td>
-                        <td contenteditable="true">@fat</td>
-                        <td contenteditable="true">Thornton</td>
-                        <td contenteditable="true">@fat</td>
-                        <td contenteditable="true">@mdo</td>
-                        <td contenteditable="true">@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td contenteditable="true">Larry</td>
-                        <td contenteditable="true">the Bird</td>
-                        <td contenteditable="true">@twitter</td>
-                        <td contenteditable="true">the Bird</td>
-                        <td contenteditable="true">@twitter</td>
-                        <td contenteditable="true">@mdo</td>
-                        <td contenteditable="true">@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td contenteditable="true">Larry</td>
-                        <td contenteditable="true">the Bird</td>
-                        <td contenteditable="true">@twitter</td>
-                        <td contenteditable="true">the Bird</td>
-                        <td contenteditable="true">@twitter</td>
-                        <td contenteditable="true">@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td contenteditable="true">Larry</td>
-                        <td contenteditable="true">the Bird</td>
-                        <td contenteditable="true">@twitter</td>
-                        <td contenteditable="true">the Bird</td>
-                        <td contenteditable="true">@twitter</td>
-                        <td contenteditable="true">@mdo</td>
-                        <td contenteditable="true">@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td contenteditable="true">Larry</td>
-                        <td contenteditable="true">the Bird</td>
-                        <td contenteditable="true">@twitter</td>
-                        <td contenteditable="true">the Bird</td>
-                        <td contenteditable="true">@twitter</td>
-                        <td contenteditable="true">@mdo</td>
-                        <td contenteditable="true">@mdo</td>
-                    </tr>
+                        ${cardItems}
+                      
                     </tbody>
                 </table>
                 <button class="rig__submit" type="submit">save</button>
